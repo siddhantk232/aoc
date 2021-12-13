5e72f2f94252ba688449ee92b48639b420a7aa30
siddhantCodes <siddhantk232@gmail.com>
2021-12-13 13:55:08 +0530

    day 02 part 2

--- 2021/day-02/sol.hs
+++ 2021/day-02/sol.hs
@@ -1,14 +1,14 @@
 module Day02 where
 
-newtype State = State (Int, Int)
+newtype State = State (Int, Int, Int)
   deriving (Show)
 
 data Command = Up Int | Down Int | Forward Int
 
 solve :: State -> Command -> State
-solve (State (pos, depth)) (Up val)      = State (pos, depth - val)
-solve (State (pos, depth)) (Down val)    = State (pos, depth + val)
-solve (State (pos, depth)) (Forward val) = State (pos + val, depth)
+solve (State (pos, depth, aim)) (Up val)      = State (pos, depth, aim - val)
+solve (State (pos, depth, aim)) (Down val)    = State (pos, depth, aim + val)
+solve (State (pos, depth, aim)) (Forward val) = State (pos + val, depth + (aim * val), aim)
 
 parseCommand :: [String] -> Command
 parseCommand ["up", n]      = Up (read n)
@@ -17,13 +17,13 @@ parseCommand ["forward", n] = Forward (read n)
 parseCommand n              = undefined
 
 prod :: State -> Int
-prod (State(pos, depth)) = pos * depth
+prod (State(pos, depth, _)) = pos * depth
 
 
 main = do
   input <- readFile "input.text"
   let commands =  map (parseCommand . words) . lines $ input
-  let result = foldl solve (State (0, 0)) commands
+  let result = foldl solve (State (0, 0, 0)) commands
   print $ prod result
   
 
